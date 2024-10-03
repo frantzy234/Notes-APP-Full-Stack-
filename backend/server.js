4783360369a21173b9d18293a3139947a9c17ec5
Frank <frank@Franks-MacBook-Air.local>
2024-10-03 09:37:12 -0400

    first commit

---
+++ backend/server.js
@@ -0,0 +1,55 @@
+const express= require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const cors = require('cors')
+const path = require("path");
+const cookieParser = require('cookie-parser')
+const connectToDb = require('./config/connectToDb')
+connectToDb()
+const notesController = require('./controllers/notesController')
+const usersController = require('./controllers/usersController')
+
+// ------->------->-------> Imports
+// ------->------->-------> Middleware
+const ensureLoggedIn = require('./config/ensureLoggedin')
+app.use(express.json());
+//  data -> json
+
+app.use(require('./config/checkToken'))
+app.use(cors({
+    origin:true,
+    credentials: true
+  }))
+// CORS: CrossOriginResourceSharing
+
+// app.use(cookieParser())
+// cookieParser: npm i cookie-parser *
+
+
+
+
+// ------->------->-------> Routes
+app.get("/notes", notesController.fetchNotes);
+// +++++++++++++ {READ} ++++++++++++++
+
+app.get("/notes/:id", notesController.fetchNote);
+// +++++++++++++ {READ} ++++++++++++++
+
+app.post("/notes", notesController.createNote);
+
+// +++++++++++++ {CREATE} ++++++++++++++
+app.put("/notes/:id", notesController.updateNote);
+// +++++++++++++ {UPDATE} ++++++++++++++
+
+app.delete("/notes/:id", notesController.deleteNote);
+// +++++++++++++ {DELETE} ++++++++++++++
+// -------------------------------------------------------[userRoutes]
+app.post('/api/users', usersController.create);
+app.post('api/users/login', usersController.login);
+app.get('api/users/check-token', ensureLoggedIn, usersController.checkToken)
+
+
+// ------->------->-------> Server
+app.listen(PORT,()=>{
+    console.log(`ServerConnectedOn: ${PORT}`)
+})
